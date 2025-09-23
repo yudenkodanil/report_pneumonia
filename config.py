3b99b4742f1687f5dcdd4f6b75e2ac3fced0acdd
Yudenko Danil <yudenkodanil@gmail.com>
2025-09-23 18:23:36 +0900

    update

---
+++ config.py
@@ -0,0 +1,1 @@
+INPUT_FILE = 'Report060U.xlsx'
--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import pandas as pd
+from config import INPUT_FILE
 
 # Конфигурация (удобно вынести в отдельный блок)
-INPUT_FILE = 'data.xlsx'
-OUTPUT_FILE = 'report.xlsx'
-CRITERIA = {'Status': 'Approved'}  # Можно усложнить
+
 
 def main():
     # Шаг 1: Чтение данных
     try:
         # Чтение Excel-файла. sheet_name - название или индекс листа
-        df = pd.read_excel(INPUT_FILE)
+        df = pd.read_excel(INPUT_FILE, skiprows=3)
         print(f"Прочитано строк: {len(df)}")
+        print(f"Количество столбцов в файле: {len(df.columns)}")
+      
+        # # Основная информация о DataFrame
+        # print(df.shape)      # размерность (строки, столбцы)
+        # print(df.columns)    # названия столбцов
+        # print(df.info())     # подробная информация
+        # print(df.head())     # первые 5 строк
+        # print(df.tail())     # последние 5 строк
+        # # Статистика по числовым столбцам
+        # print(df.describe())
+
+        column_10 = df[9]
+        print(f"10-й столбец (индекс 9):")
+        print(f"Тип данных: {column_10.dtype}")
+        print(f"Уникальные значения: {column_10.unique()[:20]}")  # Показываем первые 20 уникал
+
+        filtered_df = df[df[9].astype(str).str.lower() == 'Белогорск']   
+        print(f"Найдено записей с 'белогорск': {len(filtered_df)}")
+
     except FileNotFoundError:
         print(f"Ошибка: Файл {INPUT_FILE} не найден.")
         return
+
+
 
 if __name__ == '__main__':
     main()
